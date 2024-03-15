6928d8f0df97028c8985830c2756ae43d9061237
THAMITHA S <THAMITHA.S@ibm.com>
2024-03-15 18:55:13 +0530

    add changes

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,19 @@
-const fs = require('fs') 
+const chalk = require('chalk')
+const getNotes = require('./notes.js')
+const { default: isURL } = require('validator/lib/isURL.js')
 
-//fs.writeFileSync('notes.txt','my name is thamitha')
-fs.appendFileSync('notes.txt',' this is to append to existing file system.')
+const msg = getNotes()
+console.log(msg)
+console.log(chalk.green.inverse.bold('Success'))
+
+
+
+
+
+
+
+
+// const add = require('./utils.js')
+
+// const sum = add(2,5)
+// console.log(sum)
