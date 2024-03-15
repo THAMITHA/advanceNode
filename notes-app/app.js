60ee6f40c4915aba056bd864db3a51675a08665b
THAMITHA S <THAMITHA.S@ibm.com>
2024-03-15 13:36:20 +0530

    add appendfilesync

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,2 +1,4 @@
 const fs = require('fs') 
-fs.writeFileSync('notes.txt','my name is thamitha')
+
+//fs.writeFileSync('notes.txt','my name is thamitha')
+fs.appendFileSync('notes.txt',' this is to append to existing file system.')
